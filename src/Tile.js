e092e44b40cd440f5a1cfc470cbcd062a5115db3
Martina Natale <martinanatale93@gmail.com>
2018-10-31 22:12:38 +0000

    need to finish classes

--- src/Tile.js
+++ src/Tile.js
@@ -21,4 +21,17 @@ class Tile
         this.status = bomb;
         return true;
     }
+
+    setBombs()
+    {
+        var titles = [arguments];
+        var bombs = arguments/ (Math.random()* 20);
+        var i = 0;
+
+        while (i < bombs) {
+            var index = Math.random()*3;
+            titles[i].hasBomb();
+            i++;
+        }  
+    }
 }
